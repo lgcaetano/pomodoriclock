00896420c21428364af1170023f69b69de10186a
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-11-20 14:47:17 -0300

    Pretty sure the project is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,38 @@ import { upperFirstLetter } from "./LengthSetter"
 export default function App(){
 
     const [seconds, setSeconds] = useState(60 * 25)
-    const playFlag = useRef(false)
-    // const [referenceTime, setReferenceTime] = useState(Date.now())
+    
+    const [playFlag, setPlayFlag] = useState(false)
+    
     const referenceTime = useRef(Date.now())
-    // console.log(referenceTime)
+   
     const startTime = useRef(60 * 25)
 
-    const [breakLength, setBreakLength] = useState(1)
-    const [sessionLength, setSessionLength] = useState(1)
+    const [breakLength, setBreakLength] = useState(5)
+    
+    const [sessionLength, setSessionLength] = useState(25)
+    
     const [mode, setMode] = useState('session')
 
     const timer = useRef([])
 
+    const audioRef = useRef()
+
     function clearAllIntervals(){
-        console.log(timer)
         timer.current.forEach(interval => clearInterval(interval))
+    }
+
+    function playPause(){
+        if(playFlag)
+            pause()
+        else
+            play(seconds)
     }
 
 
     function toggleMode(){
 
         let newStartingTime 
-
-        console.log(mode)
-
         
         setMode(prevValue => {
             
@@ -40,23 +48,21 @@ export default function App(){
                 return 'session'
             }
         })
-
+        
+        audioRef.current.play()
         setSeconds(newStartingTime)
         clearAllIntervals()
-        playFlag.current = false
+        setPlayFlag(false)
         play(newStartingTime)
     }
 
 
     function play(startingTime){
 
-        
-        if(playFlag.current)
+        if(playFlag)
             return
-        
-        // console.log("HEY")
 
-        playFlag.current = true
+        setPlayFlag(true)
 
         referenceTime.current = Date.now()
 
@@ -70,8 +76,6 @@ export default function App(){
             setSeconds(() => nextSeconds)
 
             if(nextSeconds <= 0){
-                
-                // console.log(seconds)
                 toggleMode()
             }
             
@@ -79,19 +83,24 @@ export default function App(){
     }    
 
     function pause(){
-        // setReferenceTime(Date.now())
-        // setStartTime(seconds)
-        playFlag.current = false
-        // console.log("KRLFORA")
-        // console.log(timer)
+
+        setPlayFlag(false)
         clearAllIntervals()
     }
 
     function restart(){
+
+        
         pause()
+        audioRef.current.load()
+        setMode('session')
         setSessionLength(25)
         setBreakLength(5)
+        setSeconds(60 * 25)
+
     }
+
+
 
     useEffect(() => {
         if(mode == 'session')
@@ -108,19 +117,36 @@ export default function App(){
 
     return (
         <div className="app">
+
+            <audio src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+                id="beep" ref={audioRef}></audio>
+
             <div className="lengths-container">
-                <LengthSetter type="session" length={sessionLength} changeLength={setSessionLength}></LengthSetter>
+
+                <LengthSetter type="session" length={sessionLength} changeLength={setSessionLength}
+                 playing={playFlag} key={Math.random()}></LengthSetter>
                 
-                <LengthSetter type="break" length={breakLength} changeLength={setBreakLength}></LengthSetter>
+                <LengthSetter type="break" length={breakLength} changeLength={setBreakLength}
+                 playing={playFlag} key={Math.random()}></LengthSetter>
+
             </div>
+
             <div className="timer-container">
-                <div className="timer-label">{upperFirstLetter(mode)}</div>
-                <h1 className="timer">{`${minutes >= 10 ? "" : "0"}` + minutes}:{`${modSeconds >= 10 ? "" : "0"}` + modSeconds}</h1>
+
+                <div className="timer-label" id="timer-label">{upperFirstLetter(mode)}</div>
+                <h1 className="timer" id="time-left">{`${minutes >= 10 ? "" : "0"}` + minutes}:{`${modSeconds >= 10 ? "" : "0"}` + modSeconds}</h1>
+            
             </div>
+
             <div className="buttons-container">
-                <button className="play" onClick={() => play(seconds)}><i className="fas fa-play"></i></button>
-                <button className="pause" onClick={pause}><i className="fas fa-pause"></i></button>
-                <button className="restart" onClick={restart}><i className="fas fa-redo-alt"></i></button>
+
+                <button id="start_stop" onClick={playPause}>    
+                    <div className="play" ><i className="fas fa-play"></i></div>
+                    <div className="pause" ><i className="fas fa-pause"></i></div>
+                </button>
+
+                <button className="restart" onClick={restart} id="reset"><i className="fas fa-redo-alt"></i></button>
+            
             </div>
         </div>
     )
--- src/LengthSetter.jsx
+++ src/LengthSetter.jsx
@@ -3,8 +3,13 @@ export function upperFirstLetter(string) {
 }
 
 export default function LengthSetter(props){
+
+
     return (
         <div className="length-container">
+
+            
+
 
             <div id={`${props.type}-label`}>
                 {`${upperFirstLetter(props.type)} Length`}
@@ -13,15 +18,22 @@ export default function LengthSetter(props){
 
             <div className="length-buttons">
                 <button className={`${props.type}-decrement`} onClick={() => {
-                    if(props.length > 1)
+
+                    if(props.length > 1 && !props.playing)
                         props.changeLength(props.length - 1)
+                        
                     }}>
                     <i className="fas fa-arrow-down"></i>
                 </button>
 
                 <div id={`${props.type}-length`}>{props.length}</div>
 
-                <button className={`${props.type}-increment`} onClick={() => props.changeLength(props.length + 1)}>
+                <button className={`${props.type}-increment`} onClick={() => {
+
+                    if(props.length < 59 && !props.playing)
+                        props.changeLength(props.length + 1)
+
+                }}>
                     <i className="fas fa-arrow-up"></i>
                 </button>
             </div>
